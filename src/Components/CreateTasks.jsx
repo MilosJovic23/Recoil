ff44e1badf8a8f0d8a8b1a3c730c139e946a0bb2
MilosJovic23 <milos.mj992@gmail.com>
2024-04-02 12:58:08 +0200

    * small changes

--- src/Components/CreateTasks.jsx
+++ src/Components/CreateTasks.jsx
@@ -1,16 +1,25 @@
-import { useSetRecoilState } from "recoil";
+import {useRecoilValue, useSetRecoilState} from "recoil";
 import { TasksState } from "../States/TasksState";
 import { useForm } from "react-hook-form";
 import { TaskValidator } from "../Validators/TaskValidator";
+import {useState} from "react";
 
 
 const CreateTasks= ()=>{
 
     const { register,handleSubmit,formState:{errors} } =useForm()
     const setTasks=useSetRecoilState(TasksState)
-
+    const Tasks=useRecoilValue(TasksState);
+    const [taskNameError,setTaskNameError]=useState("");
+    let taskFound=false;
     const onSubmit= (data)=>{
+        if(Tasks.includes(data.taskName)){
+            setTaskNameError("Postoji task sa tim imenom");
+            taskFound=true;
+            return;
+        }
         setTasks(tasks=>[...tasks,data.taskName])
+        setTaskNameError("")
     }
     const deleteAllTasks= ()=>{
         setTasks([])
@@ -19,6 +28,7 @@ const CreateTasks= ()=>{
     return(
         <>
             <form onSubmit={ handleSubmit(onSubmit) }>
+                {!taskFound && (<p>{taskNameError}</p>)}
                 { errors.taskName && (<p>{errors.taskName.message}</p>) }
                 <input type="text" {...register("taskName", TaskValidator )}/>
                 <button>CreateTask</button>
