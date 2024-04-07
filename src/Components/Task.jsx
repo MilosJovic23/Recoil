6b99ce956757983f6320548f8435278b031b15f0
MilosJovic23 <milos.mj992@gmail.com>
2024-04-07 18:46:58 +0200

    * small changes

--- src/Components/Task.jsx
+++ src/Components/Task.jsx
@@ -2,6 +2,7 @@ import CreateTasks from "./CreateTasks";
 import { useRecoilValue, useSetRecoilState } from "recoil";
 import { UserState } from "../States/UserState";
 import { TasksState } from "../States/TasksState";
+import {useState} from "react";
 
 
 const Task=()=>{
@@ -9,12 +10,21 @@ const Task=()=>{
     const Tasks=useRecoilValue(TasksState);
     const userData=useRecoilValue(UserState);
     const setTaskData=useSetRecoilState(TasksState);
-
+    const [editToDo,setEditToDo]=useState(false);
+    const [editTaskName,setEditTaskName]=useState("")
     const deleteTask=( taskName )=>{
 
         const filteredTasks=Tasks.filter( (task)=> task.name !== taskName )
         setTaskData(filteredTasks)
 
+    }
+    const editTask=(task,id)=>{
+        console.log(task,id)
+        setEditToDo(true)
+    }
+    const saveTask=()=>{
+        setEditToDo(false);
+        console.log(editTaskName)
     }
     return(
                 <>
@@ -26,8 +36,17 @@ const Task=()=>{
 
                                     return (
                                         <div key={index}>
-                                            <p>{ task.name }</p>
-                                            <button type="button" onClick={ () => deleteTask( task.name ) }>delete task</button>
+                                            <p>{task.name}</p>
+                                            { !editToDo ?
+                                                ( <button type="button" onClick={ editTask }>edit</button>):
+                                                (<div>
+                                                        <button type="button"  onClick={() => saveTask(task.name, task.id)}>save</button>
+                                                        <input type="text" onInput={(e)=>setEditTaskName(e.currentTarget.value)}/>
+                                                </div>
+                                                )}
+
+                                            <button type="button" onClick={() => deleteTask(task.name)}>delete task
+                                            </button>
                                         </div>
                                     )
                                 })}
