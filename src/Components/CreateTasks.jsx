3f45bce8a8726c13acf3203ada8364c9f56843dc
MilosJovic23 <milos.mj992@gmail.com>
2024-04-09 17:46:32 +0200

    * added react hook form in Task.jsx

--- src/Components/CreateTasks.jsx
+++ src/Components/CreateTasks.jsx
@@ -32,6 +32,7 @@ const CreateTasks= ()=>{
         return;
     }
 
+
     const deleteAllTasks=()=>{
         setTasks([])
     }
--- src/Components/Task.jsx
+++ src/Components/Task.jsx
@@ -39,6 +39,7 @@ const Task = () =>{
         setIsEdited(false);
 
     }
+
     return(
                 <>
                     { isEdited && (
