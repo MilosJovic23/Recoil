641b3f973b071048d41ec53c3e7cfb254fd6b79d
MilosJovic23 <milos.mj992@gmail.com>
2024-04-18 13:02:06 +0200

    * added changes to Task.jsx and login.jsx

--- src/Components/Task.jsx
+++ src/Components/Task.jsx
@@ -2,18 +2,25 @@ import CreateTasks from "./CreateTasks";
 import { useRecoilValue, useSetRecoilState } from "recoil";
 import { UserState } from "../States/UserState";
 import { TasksState } from "../States/TasksState";
-import {useState} from "react";
-import {useForm} from "react-hook-form";
-import {NewTaskNameValidator} from "../Validators/NewTaskNameValidator";
-import {TaskCategories} from "../TasksCategories/TaskCategories";
-
+import {useEffect, useState} from "react";
+import { useForm } from "react-hook-form";
+import { NewTaskNameValidator } from "../Validators/NewTaskNameValidator";
+import { TaskCategories } from "../TasksCategories/TaskCategories";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import CommentForm from "./CommentForm";
+import GetAllComments from "./GetAllComments";
+import GetAllCommentsforPost from "./GetAllComments";
+import GetAllCommentsForPost from "./GetAllComments";
 const Task = () =>{
     const Tasks=useRecoilValue(TasksState);
     const userData=useRecoilValue(UserState);
     const setTaskData=useSetRecoilState(TasksState);
-    const [isEdited,setIsEdited]=useState(false);
-    const {register,handleSubmit,formState:{errors}}=useForm()
-    const [id,setId]=useState("")
+    const {register,
+        handleSubmit,
+        formState:{errors},
+        reset
+        }=useForm()
+    const [editTaskId,setEditTaskId]=useState()
 
     const deleteTask=( taskName )=>{
 
@@ -22,63 +29,108 @@ const Task = () =>{
 
     }
 
-    const editTask=( id )=>{
-        setIsEdited(true);
-        setId( id );
+    const newCommentPosted=(data)=>{
+
+
+               const tasksWithComments=Tasks.map(( task )=>{
+
+                   let existingComments=[]
+                   if(Array.isArray(task.comments)){
+                       existingComments =task.comments;
+                   }
+                  return task.id ===  data.taskId ?
+                        {...task,comments:[...existingComments,data.comment]}
+                        :
+                        task
+               });
+                setTaskData(tasksWithComments)
     }
-    const onSubmit=( data )=>{
+    const updateTask=( data )=>{
+        setEditTaskId(null)
 
         const updatedTasks=Tasks.map(( task )=>{
-
-            return task.id === id ?
-                { "id":id,"name":data.newTaskName,"category":data.taskCategory }
-                : task ;
-
+            return task.id === editTaskId ?
+                { "id":data.taskId,"name":data.newTaskName,"category":data.taskCategory }
+                :
+                task
         });
         setTaskData( updatedTasks );
-        setIsEdited(false);
-
     }
+
+
+    useEffect(() => {
+        if( editTaskId !== undefined ){
+            const task=Tasks.find( t=> t.id === editTaskId );
+
+            if(task){
+                reset(
+                    {
+                        taskId: task.id,
+                        newTaskName : task.name,
+                        taskCategory:task.category
+                    }
+                )
+            }
+        }
+    }, [editTaskId,reset,Tasks]);
 
     return(
                 <>
-                    { isEdited && (
-                        <form onSubmit={ handleSubmit(onSubmit) }>
-                            { errors.newTaskName && ( <p>{ errors.newTaskName.message }</p> )}
-                            <input {...register("newTaskName", NewTaskNameValidator )}></input>
-                            <select {...register("taskCategory")} >{ TaskCategories.map((task) => {
-                                return <option>{task}</option>})}</select>
-                            <button>save</button>
-                        </form>
-
-                    )}
+                    {
+                        userData.LoggedIn && (<div className="Task m-5">
+                            <CreateTasks/>
+                        </div>)
+                    }
                     { userData.LoggedIn &&
                         (
-                            <div>
 
-                            { Tasks.map((task,index)=>{
+                            <div className="taskHolder mt-2 align-items-center">
+
+                                {Tasks.map((task, index) => {
 
                                     return (
-                                        <>
-                                            <div key={index}>
-                                                <p>{task.name}</p>
-                                                <button type="button" onClick={() => editTask(task.id)}>edit</button>
-                                                <button type="button" onClick={() => deleteTask(task.name)}>delete
-                                                    task
-                                                </button>
-                                            </div>
+                                        <div className="singleTask" key={index}>
 
-                                            <form key={index}>
-                                                <input type="text" value={task.name}/>
-                                            </form>
-                                        </>
+                                            { editTaskId === task.id ? (
+                                                <form className="updateTask" onSubmit={ handleSubmit(updateTask) }>
+                                                    {errors.newTaskName && (<p>{errors.newTaskName.message}</p>)}
+                                                    <input {...register("taskId")} type="hidden"
+                                                           defaultValue={task.id}/>
+                                                    <input {...register("newTaskName", NewTaskNameValidator)}
+                                                           type="text"  defaultValue={task.name}/>
+                                                    <select {...register("taskCategory")}
+                                                            defaultValue={task.category}>{TaskCategories.map((category) => {
+                                                        return <option>{category}</option>
+                                                    })}
+                                                    </select>
+                                                    <button>update Task</button>
+                                                </form>
+                                            ) : (
+                                                <span>
+                                                    <h2 className="m-0 p-0"
+                                                        onClick={() => setEditTaskId( task.id )}>{ task.name }</h2>
+                                                    <p className="mb-3 p-0"
+                                                       onClick={() => setEditTaskId( task.id )}>{ task.category }</p>
+                                                    <button className="p-1 btn btn-dark rounded-1 btn-sm" type="button" onClick={() => deleteTask( task.name )}>delete
+                                                        task
+                                                    </button>
+                                                    <CommentForm task={ task }  callbackPostComment = { newCommentPosted }/>
+                                                    <GetAllCommentsForPost  comments={task.comments} taskId={task.id}/>
+
+                                                </span>
+
+                                            )
+                                            }
+
+
+                                        </div>
 
                                     )
-                            })}
+                                })}
 
-                                <CreateTasks/>
 
                             </div>
+
                         )}
                 </>
 
--- src/Components/login.jsx
+++ src/Components/login.jsx
@@ -3,6 +3,7 @@ import { UserState } from "../States/UserState";
 import { useForm } from "react-hook-form";
 import { EmailValidator } from "../Validators/EmailValidator";
 import { PasswordValidator } from "../Validators/PasswordValidator";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 const Login=()=>{
@@ -27,19 +28,21 @@ const Login=()=>{
 
     return(
         <>
+            <div className="d-flex justify-content-center ">
+                {!userData.LoggedIn? (
+                    <form className="w-25 gap-2 d-flex flex-column justify-content-center align-items-center " onSubmit={ handleSubmit(onSubmit) }>
+                        { errors.email && (<p>{errors.email.message}</p>) }
+                        <input className="form-control" type="text" placeholder="email" {...register("email", EmailValidator )} />
+                        { errors.password && (<p>{errors.password.message}</p>) }
+                        <input className="form-control" type="password" placeholder="password" {...register("password", PasswordValidator  )} />
+                        <button className="btn btn-dark">Login</button>
+                    </form>
+                ):(
+                    <button className="btn btn-dark" onClick={ HandleLogout }>Logout</button>
+                )
+                }
+            </div>
 
-            {!userData.LoggedIn? (
-            <form onSubmit={ handleSubmit(onSubmit) }>
-                { errors.email && (<p>{errors.email.message}</p>) }
-                <input type="text" placeholder="email" {...register("email", EmailValidator )} />
-                { errors.password && (<p>{errors.password.message}</p>) }
-                <input type="password" placeholder="password" {...register("password", PasswordValidator  )} />
-                <button  >Login</button>
-            </form>
-            ):(
-            <button onClick={ HandleLogout }>Logout</button>
-            )
-            }
 
 
         </>
